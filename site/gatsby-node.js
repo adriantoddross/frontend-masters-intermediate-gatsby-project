4612c5f3ffae2df1839bd4dc59f954014512e9a9
Adrian Ross <adriantoddross@gmail.com>
2021-08-13 11:54:03 -0400

    Add author & book data types

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,14 +1,49 @@
-exports.createPages = ({ actions }) => {
-  const { createPage } = actions;
+const authors = require('./src/data/authors.json');
+const books = require('./src/data/books.json');
 
-  createPage({
-    path: "/custom",
-    component: require.resolve("./src/templates/custom.js"),
-    context: {
-      title: "A Custom Page!",
-      meta: {
-        description: "A custom page with context.",
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode } = actions;
+
+  authors.forEach((author) => {
+    createNode({
+      ...author,
+      id: createNodeId(`author-${author.slug}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Author',
+        content: JSON.stringify(author),
+        contentDigest: createContentDigest(author),
       },
-    },
+    });
   });
+
+  books.forEach((book) => {
+    createNode({
+      ...book,
+      id: createNodeId(`book-${book.isbn}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Book',
+        content: JSON.stringify(book),
+        contentDigest: createContentDigest(book),
+      },
+    });
+  });
+
+  exports.createPages = ({ actions }) => {
+    const { createPage } = actions;
+
+    createPage({
+      path: '/custom',
+      component: require.resolve('./src/templates/custom.js'),
+      context: {
+        title: 'A Custom Page!',
+        meta: {
+          description: 'A custom page with context.',
+        },
+      },
+    });
+  };
 };
